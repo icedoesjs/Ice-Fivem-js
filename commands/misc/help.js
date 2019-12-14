989f912133b1ae0e9d6ca66d7c32f45940b9f378
iceyym8 <40555230+iceyym8@users.noreply.github.com>
2019-12-14 15:54:31 -0500

    Add files via upload

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -6,45 +6,47 @@ module.exports = {
     category: "misc",
     description: "Returns all commands, or one specific command info",
     usage: "[command | alias]",
-    run: async (client, message, args, config) => {
+    run: async (client, message, args, config, language) => {
         if (args[0]) {
             return getCMD(client, message, args[0]);
         } else  {
-            return menu(client, message, config)
+            return menu(client, message, config, language)
         }
     }
 }
 
-function menu(client, message, config){
+function menu(client, message, config, language){
     const embed = new RichEmbed()
     .setColor(`${config.color}`)
     .setThumbnail(`${config.logo}`)
-    .setFooter(`${config.shortname} bot made by IceyyM8 | github.com/iceyym8`)
-    .setDescription(`Thanks for using ${config.shortname} bot, My prefix is ${config.prefix1} and my developer prefix is ${config.devprefix}. View my commands Below!`)
-    .addField(`Admin Commands`," `ban` `kick` `purge` `setlvl`")
-    .addField(`Misc Commands`," `binfo` `help` `steam` `u-info`")
-    .addField(`Profile Commands`," `age` `commend` `gender` `hex` `rpname` `profile @user`")
-    .addField(`Developer Commands (${config.devprefix})`, "`kill`, `restart`")
-    .addField(`TxAdmin Commands`, "`restartfx` `fxconfig` `query` `restartrsc` `kickid` `announcefx`")
+    .setFooter(`${config.shortname} bot ${language.madeby} | github.com/iceyym8`)
+    .setDescription(`${language.thanksforusing} ${config.shortname} bot, Prefix = ${config.prefix1}. ${language.viewmycmdsbelow} ${config.prefix1}help cmdname`)
+    .addField(`${language.admincmds}`," `ban` `kick` `purge` `setlvl` `announce`")
+    .addField(`${language.misccmds}`," `binfo` `help` `steam` `u-info`")
+    .addField(`${language.profilecmds}`," `age` `commend` `gender` `hex` `rpname` `profile @user`")
+    .addField(`${language.devcmds} (${config.devprefix})`, "`kill`, `restart`")
+    .addField(`${language.servercmds}`, "`ip` `players` `rules`")
+    .addField(`${language.txadmincmds}`, "`restartfx` `fxconfig` `query` `restartrsc` `kickid` `announcefx`")
     if (config.txadmin == "true") {
         return message.channel.send(embed)
     } else {
     const embed = new RichEmbed()
     .setColor(`${config.color}`)
     .setThumbnail(`${config.logo}`)
-    .setFooter(`${config.shortname} bot made by IceyyM8 | github.com/iceyym8`)
-    .setDescription(`Thanks for using ${config.shortname} bot, My prefix is ${config.prefix1} and my developer prefix is ${config.devprefix}. View my commands Below!`)
-    .addField(`Admin Commands`, " `ban` `kick` `purge` `setlvl`")
-    .addField(`Misc Commands`, " `binfo` `help` `steam` `u-info`")
-    .addField(`Profile Commands`, " `age` `commend` `gender` `hex` `rpname` `profile @user`")
-    .addField(`Developer Commands (${config.devprefix})`, "`kill`, `restart`")
+    .setFooter(`${config.shortname} bot ${language.madeby} | github.com/iceyym8`)
+    .setDescription(`${language.thanksforusing} ${config.shortname} bot, Prefix = ${config.prefix1}. ${language.viewmycmdsbelow} ${config.prefix1}help cmdname`)
+    .addField(`${language.admincmds}`, " `ban` `kick` `purge` `setlvl` `announce`")
+    .addField(`${language.misccmds}`, " `binfo` `help` `steam` `u-info`")
+    .addField(`${language.profilecmds}`, " `age` `commend` `gender` `hex` `rpname` `profile @user`")
+    .addField(`${language.servercmds}`, "`ip` `players` `rules`")
+    .addField(`${language.devcmds} (${config.devprefix})`, "`kill`, `restart`")
     message.channel.send(embed)
     }
     
 }
 
 
-function getCMD(client, message, input){
+function getCMD(client, message, input, language){
     const embed = new RichEmbed()
 
     const cmd = client.commands.get(input.toLowerCase()) || client.commands.get(client.aliases.get(input.toLowerCase()));
