2b1b79de5c125a8ba07ea60d6f5c2b7fcbaa8cac
iceyym8 <40555230+iceyym8@users.noreply.github.com>
2020-03-08 17:26:42 -0400

    Update players.js

--- commands/server/players.js
+++ commands/server/players.js
@@ -1,20 +1,25 @@
 const { RichEmbed } = require('discord.js')
-const { getServerInfo } = require('fivem-api')
+const dig = require('gamedig')
 module.exports = {
-        name: "players",
-        category: "server",
-        description: "Check how many players are on our server",
-        run: async (client, message, args, config, language) => {
+    name: "players",
+    category: "server",
+    description: "See our online players!",
+    run: async(client, message, args, config, language) => {
         await message.delete();
-
-        var online = await getServerInfo(`${config.ip}`)
-        var max = online.infos.vars.sv_maxClients
-        let embed = new RichEmbed()
-            .setAuthor(`${config.shortname} ${language.playersonline}`)
-            .setThumbnail(`${config.logo}`)
-            .setColor(`${config.color}`)
-            .addField(`Online`, `${online.players.length}/${max}`, true)
-            .setFooter(`IP: ${config.ip}`)
-        message.channel.send(embed)
+        return dig.query({
+            type: 'fivem',
+            host: config.ip,
+            port: config.port,
+            socketTimeout: 5000,
+            udpTimeout: 10000
+        }).then((info) => {
+            const embed = new RichEmbed()
+                .setAuthor(info.name)
+                .setColor(config.color)
+                .addField(`Online`, `${info.raw.clients}/${info.raw.sv_maxclients}`, true)
+            message.channel.send(embed)
+        }).catch((error) => {
+            message.reply(`It seems the server is **offline!**`)
+        })
     }
 }
