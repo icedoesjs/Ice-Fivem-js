ff6f5dba3ad250e36c46598a7672469c8198ef0c
iceyym8 <40555230+iceyym8@users.noreply.github.com>
2020-03-08 17:45:38 -0400

    Update install-depend.bat

--- install-depend.bat
+++ install-depend.bat
@@ -1,5 +1,27 @@
 @echo off 
-npm install better-sqlite3 --save
-npm install discord.js --save
-npm install ascii-table --save
+rem Just a basic dependecie installer lmao
+echo Welcome and thanks for using Ice-Fivem-JS 
+ping localhost -n 2 >nul
+cls 
+GOTO :start
+
+:start 
+SET /P UPDATE=Are you sure you want to update/install all dependecies? (Y/N)? 
+IF /I %UPDATE% NEQ "Y" GOTO updater 
+IF /I %UPDATE% NEQ "N" GOTO closer
+
+:updater 
+cls 
+echo Please wait while everything installs....
+ping localhost -n 2 >nul
+cls
 npm install 
+
+:closer 
+echo Thanks for using Ice-Fivem-JS 
+ping localhost -n 2 >nul
+cls 
+echo I will now close 
+ping localhost -n 2 >nul 
+cls 
+exit
